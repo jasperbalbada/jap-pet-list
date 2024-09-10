ea518ead2a8b115cabc6580d15d0e49bc05bc2a6
Jasper Balbada <jappy.balbada@symph.co>
2024-09-10 10:42:19 +0800

    [feature][levdev] - improved pet list page. implemented add, edit, delete pet. implement upload photo feature

--- jap-pet-list/src/components/PetProfile.tsx
+++ jap-pet-list/src/components/PetProfile.tsx
@@ -1,23 +1,38 @@
-import Link from "next/link";
-import { PhotoIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon, PhotoIcon } from "@heroicons/react/24/outline";
 
-const PetProfile: React.FC = () => {
+
+const PetProfile: React.FC <{ 
+  pets: { id: number; name: string; link: string; photo: string | null }[], 
+  onPetClick: (pet: any) => void }> = ({ pets, onPetClick }) => {
   return (
-    <div className="bg-white container rounded-lg p-4 max-w-sm mx-auto shadow-md">
-      <div className="flex flex-col space-y-4">
-        <div className="flex items-center space-x-4">
-          <PhotoIcon className="h-6 w-6 text-gray-500" />
-          <Link href="/" className="text-gray-700 hover:text-blue-600 text-lg">
-            Pet 1
-          </Link>
+    <div className="bg-white rounded-lg p-2 w-96 shadow-md">
+      {pets.length === 0 ? (
+        <div className="text-center text-gray-500 py-2">
+          No pets on your list yet
         </div>
-        <div className="flex items-center space-x-4">
-          <PhotoIcon className="h-6 w-6 text-gray-500" />
-          <Link href="/" className="text-gray-700 hover:text-blue-600 text-lg">
-            Pet 2
-          </Link>
+      ) : (
+        <div className="flex flex-col divide-y divide-gray-200">
+          {pets.map((pet) => (
+            <div
+              key={pet.id}
+              className="hover:bg-gray-100 p-2 block cursor-pointer"
+              onClick={() => onPetClick(pet)}
+            >
+              <div key={pet.id} className="flex items-center justify-between hover:bg-gray-100 p-2">
+                <div className="flex items-center space-x-4">
+                  {pet.photo ? (
+                    <img src={pet.photo} alt={pet.name} className="h-10 w-10 rounded-full object-cover" />
+                  ) : (
+                    <PhotoIcon className="h-6 w-6 text-gray-500" />
+                  )}
+                  <h1 className="text-gray-700 text-lg">{pet.name}</h1>
+                </div>
+                <ChevronRightIcon className="h-6 w-6 text-gray-500" />
+              </div>
+            </div>
+          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 };
---
+++ jap-pet-list/src/constants/routes.ts
@@ -0,0 +1,11 @@
+export const routes = {
+  MAIN: '/',
+  PET_LIST: '/petlist',
+  REROLL_SIMULATOR: 'reroll',
+}
+
+export const mainRoutes = [
+  { link: routes.MAIN, label: 'ｌｅｖ／ｄｅｖ'},
+  { link: routes.PET_LIST, label: 'Pet List'},
+  { link: routes.REROLL_SIMULATOR, label: 'Reroll Simulator'},
+];
